5d526fde62e2516fcc4714fbeb7375edef6c4f0b
NoCluePS <pijus.serap@gmail.com>
2025-03-29 19:09:34 +0200

    init supabase

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,6 +1,12 @@
 import Header from "@/components/header";
 import { Outlet } from "react-router";
 import { Toaster } from "@/components/ui/sonner";
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+  "https://xmfozbvukwgcisiiwnkf.supabase.co",
+  process.env.SUPABASE_ANON_KEY ?? "",
+);
 
 export default function Layout() {
   return (
